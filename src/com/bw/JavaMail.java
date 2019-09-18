845c921dd9002837939c37fbd45f44cd65771bca
1013480363 <l1013480363@163.com>
2019-09-18 21:39:29 +0800

    最新

--- src/com/bw/JavaMail.java
+++ src/com/bw/JavaMail.java
@@ -31,7 +31,6 @@ public class JavaMail {
 		Session session = Session.getInstance(props, auth);
 		System.out.println(222222);
 		System.out.println(33);
-		System.out.println(4444444);
 		// 2.创建一个Message，它相当于是邮件内容
 		MimeMessage message = new MimeMessage(session);
 		// 设置发送者
